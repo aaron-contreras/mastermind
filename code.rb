cd3d556c4e80818309cc14987d2ea8ca1163a550
Aaron Contreras <aaronlcaq@gmail.com>
2020-06-13 09:54:13 -0500

    Clean up with rubocop

--- code.rb
+++ code.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# Represents the code pegs that build a pattern
 class Code < Peg
   def to_s
-    colorize(BACKGROUND_COLORS[color], CODE_PEG_CHARACTER) 
+    colorize(BACKGROUND_COLORS[color], CODE_PEG_CHARACTER)
   end
 end
--- key.rb
+++ key.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Represents the feedback pegs for a given pattern
 class Key < Peg
   def to_s
     colorize(FOREGROUND_COLORS[color], KEY_PEG_CHARACTER)
